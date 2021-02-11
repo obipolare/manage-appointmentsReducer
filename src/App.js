3da503cd84e82a9ab6ebe0766e9580c5f3dcceb3
obipolare <codarko15@gmail.com>
2021-02-10 21:18:24 -0500

    usingReducer-appointments

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useReducer } from "react";
+import Form from "./components/Form";
+import Date from "./components/Date";
+import dateReducer from "./components/dateReducer";
 
-function App() {
+const init = () => {
+  return JSON.parse(localStorage.getItem("date")) || [];
+};
+
+const App = () => {
+  const [dates, dispatch] = useReducer(dateReducer, [], init);
+
+  console.log(dates);
+
+  useEffect(() => {
+    localStorage.setItem("date", JSON.stringify(dates));
+  }, [dates]);
+
+  const handleDelete = (dateId) => {
+    dispatch({
+      type: "delete",
+      payload: dateId,
+    });
+  };
+  const appointmentTitle =
+    dates.length === 0 ? "Add an Appointment!" : "Manage Appointments";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="m-8 text-4xl font-bold text-center">
+        Manage Appointments App
+      </h1>
+      <section className="grid grid-cols-2">
+        <article>
+          <Form dispatch={dispatch} />
+        </article>
+        <article>
+          <h2 className="mb-8 text-4xl font-bold text-center">
+            {appointmentTitle}
+          </h2>
+          <div className="p-4 bg-gray-800 rounded">
+            {dates.map((date) => (
+              <Date key={date.id} {...date} handleDelete={handleDelete} />
+            ))}
+          </div>
+        </article>
+      </section>
     </div>
   );
-}
+};
 
 export default App;
